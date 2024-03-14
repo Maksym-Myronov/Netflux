a0b54eacfb67f5060d23249896810a02244bed0e
Maksym <maksym.myronov2018@gmail.com>
2024-03-14 21:50:57 +0200

    [MP-0008] - create watchlist and add new redux

---
+++ src/pages/components/Watchlist/index.tsx
@@ -0,0 +1,40 @@
+import React, {useEffect} from 'react';
+import {useSelector} from "react-redux";
+import WatchlistCards from "./WatchlistCards";
+// Images
+import fatalImages from '../../../assets/images/Fatal Error.svg'
+// Styles
+import styles from './index.module.scss'
+
+interface RootState {
+    watchList: object
+}
+export const Watchlist: React.FC = () => {
+
+    const watchlist = useSelector((state: RootState) => state.watchList)
+
+    useEffect(() => {
+        console.log(watchlist)
+    }, [watchlist])
+
+    return (
+        <div className={styles.watchlist}>
+            {watchlist && watchlist.movies && watchlist.movies.length === 0 ? (
+                    <div className={styles.watchlist__error__card}>
+                        <img src={fatalImages} alt="images" />
+                        <h1 className={styles.watchlist__error__title}>OOPS!</h1>
+                        <h1>You dont have ane movies on your watchlist</h1>
+                    </div>
+                ) : watchlist && watchlist.movies && watchlist.movies.map((item) => (
+                    <WatchlistCards
+                        key={item.id}
+                        id={item.id}
+                        posterPath={item.poster_path}
+                        title={item.title}
+                        rating={item.voteAverage}
+                    />
+                ))
+            }
+        </div>
+    );
+};
---
+++ src/store/watchListSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    movies: []
+}
+
+const watchListSlice = createSlice({
+    name: 'watchList',
+    initialState,
+    reducers: {
+        updateMovies: (state, action) => {
+            action.payload.forEach((item) => {
+                const isDuplicate = state.movies.some((items) => items.id === item.id)
+                if(!isDuplicate) {
+                    return state.movies.push(...action.payload);
+                }
+            })
+        },
+        removeMoviesFromArray: (state, action) => {
+            state.movies = state.movies.filter(item => item.id !== action.payload)
+        }
+    }
+})
+
+export default watchListSlice.reducer;
+export const { updateMovies, removeMoviesFromArray } = watchListSlice.actions;
